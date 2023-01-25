48bd3b0c9addaa41ed2631a3328fcd5e6a37f892
Stephen Monaghan <S00234043@atu.ie>
2023-01-25 10:49:04 +0000

    finish question 1

--- revision question 1/Program.cs
+++ revision question 1/Program.cs
@@ -7,10 +7,15 @@
     {
         static void Main(string[] args)
         {
-            
 
-            PrintIntegers();
-            PrintIntegersWhile();
+
+           // PrintIntegers();
+          //  PrintIntegersWhile();
+
+           // PrintIntegersDo();
+
+            PrintDescendingFor();
+            PrintExcludingFor();
         }
 
         static void PrintIntegers()
@@ -19,14 +24,14 @@
             {
                 Console.WriteLine(i);
             }
-        }    
+        }
 
 
 
-        static void PrintIntegersWhile()
+        static void PrintIntegersWhile()// usig a while loop to preform a print function numbers 40 to 60
         {
             int x = 40;
-            while(x<=60)
+            while (x <= 60)
             {
                 Console.WriteLine(x);
                 x++;
@@ -34,9 +39,40 @@
 
         }
 
+        static void PrintIntegersDo()
+        {
+            int x = 40;
+            do
+            {
+                Console.WriteLine(x);
+            }
+            while (x <= 60);
+
+
+        }
 
 
 
 
-    }
+        static void PrintDescendingFor()
+        {
+            for (int i = 60; i >= 40; i--) /// this prints the numbers from 4o to 60 decending
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+        static void PrintExcludingFor()
+        {
+            for (int i = 40; i <= 60; i++) /// this prints the numbers from 4o to 60 decending
+            {
+                if ()
+            }
+        }
+
+
+        
+
+
+     }
 }
